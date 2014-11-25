e9e43eb22dcf5e5653bf0af9ac165dfe138c6e81
h1994st <h1994st@gmail.com>
2014-11-25 23:27:52 +0800

    Assignment 2

---
+++ Assignment 2/2.fork.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+
+#define err_sys(info) { \
+    fprintf(stderr, "%s: %s\n", info, strerror(errno)); \
+}
+
+int g = 521;
+char buf[] = "hello, to stdout\n";
+
+int main(int argc, const char *argv[]) {
+    int l;
+    pid_t pid;
+    
+    l = 125;
+    
+    if (write(STDOUT_FILENO, buf, sizeof(buf) - 1) != sizeof(buf) - 1) {
+        err_sys("write error");
+    }
+    printf("before call fork\n");
+    
+    pid = fork();
+    if (pid < 0) {
+        err_sys("fork error");
+    } else if (0 == pid) {
+        // 子进程
+        g--;
+        l++;
+        printf("in child process\n");
+        printf("pid=%d, global=%d, local=%d\n", getpid(), g, l);
+    } else {
+        // 父进程
+        printf("in parent process\n");
+        printf("pid=%d, global=%d, local=%d\n", getpid(), g, l);
+    }
+    
+    exit(0);
+}
---
+++ Assignment 2/README.md
@@ -0,0 +1,28 @@
+第二次作业
+=========
+
+## Is the COW technique same with running in the parent's address space?
+
+COW(Copy on Write) 
+
+## What are the differences between fork and vfork? Write a program to verify your answer.
+
+#### 用法
+
+```bash
+$ 
+```
+
+## Write a program to show the child process wait the parent process with a block style and a noblock style to terminate. Then show the changing of memory layout.
+
+#### 用法
+
+```bash
+$ 
+```
+
+## How can you write a shell? Practice it.
+
+#### 思路
+
+
