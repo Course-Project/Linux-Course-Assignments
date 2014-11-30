60ba7bc1031572a255c663be012fb06dd09f863a
h1994st <h1994st@gmail.com>
2014-11-30 23:07:51 +0800

    finish shell

--- Assignment 2/4.shell.c
+++ Assignment 2/4.shell.c
@@ -15,22 +15,37 @@
 int main(int argc, const char *argv[]) {
     char str[MAX_SIZE];
     pid_t pid;
-    while (scanf("%[^\n]", str) != EOF) {
+    int status;
+    char *cmd, *rest, *path;
+    while (1) {
+        path = getcwd(NULL, 0);
+        printf("guest@guest:%s$ ", path);
+        scanf("%[^\n]", str);
         getchar(); // 读取回车
         
-        if (strcmp(str, "exit") == 0) break;
+        if (strcmp(str, "exit") == 0) break; // 判断是否退出
+        
+        cmd = strtok_r(str, " ", &rest); // 分割字符串，用于判断cd命令
+        
+        if (strcmp(cmd, "cd") == 0) {
+            if (rest) chdir(rest);
+            continue;
+        }
+        
+        if (rest) *(rest - 1) = ' ';
         
         pid = vfork();
         if (pid < 0) {
             err_sys("vfork error");
         } else if (pid == 0) {
-            if (execlp("ls", "ls", "/Users/tomhu/Desktop", NULL) < 0) {
-                fprintf(stderr, "sh: command not found: %s\n", "");
-            }
+            execl("/bin/sh", "sh", "-c", str, NULL);
             _exit(EXIT_SUCCESS);
+        } else {
+            waitpid(pid, &status, WUNTRACED); // 父进程等待子进程
         }
         
         memset(str, 0, sizeof(str));
+        free(path);
     }
     exit(EXIT_SUCCESS);
 }
